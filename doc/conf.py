586f9cb47d18ca4b143df119edf3386d9bf17255
John Parejko <parejkoj@uw.edu>
2021-09-21 10:58:34 -0700

    Modernize sphinx configuration

--- doc/conf.py
+++ doc/conf.py
@@ -3,11 +3,10 @@
 This configuration only affects single-package Sphinx documenation builds.
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.meas.deblender
+from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
 
-
-_g = globals()
-_g.update(build_package_configs(
-    project_name='meas_deblender',
-    version=lsst.meas.deblender.version.__version__))
+project = "utils"
+html_theme_options["logotext"] = project     # noqa: F405, unknown name
+html_title = project
+html_short_title = project
+doxylink = {}
